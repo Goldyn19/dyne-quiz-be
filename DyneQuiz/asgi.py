5c3f8a904e666ea1407b74696d1c0931acade71c
Goldyn19 <nwaezegolden2002@gmail.com>
2025-08-11 11:27:45 +0100

    initialized asgi

--- DyneQuiz/asgi.py
+++ DyneQuiz/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from quiz.routing import  websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DyneQuiz.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
